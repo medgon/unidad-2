801e7a7d2c057f51f3cdf2f9fa90845fcbffb446
medgon <143623081+medgon@users.noreply.github.com>
2023-09-06 08:49:27 -0600

    Add files via upload

---
+++ listadetareas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variable.pkg1.fund.prog;
+
+/**
+ *
+ * @author Usuario
+ */
+public class listadetareas {
+    
+    public stattic void main (String [] args) 
+    
+     String cadenaTexto = "lista de tareas!!! %% $$$":
+    
+    
+}
