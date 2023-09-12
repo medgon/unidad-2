7e4ed94b90d00901f2e3e904474d5915ea28d1a6
medgon <143623081+medgon@users.noreply.github.com>
2023-09-11 18:09:59 -0600

    Update listadetareas.java

--- listadetareas.java
+++ listadetareas.java
@@ -8,11 +8,22 @@ package variable.pkg1.fund.prog;
  *
  * @author Usuario
  */
+//psvm para main
 public class listadetareas {
+    Lista de Tareas() {
+                String T1, T2, T3, T4, T5;
+        T1 = "Apagar mi alarma";
+        T2 = "Bañarme";
+        T3 = "Cambiarme";
+        T4 = "Desayunar"
+        T5 = "Lavarme los dientes"; 
+        System.out.println("Mi tarea 1 es: "+T1);
+        System.out.println("Mi tarea 2 es: "+T2);
+        System.out.println("Mi tarea 3 es : "+T3);
+        System.out.println("Mi tarea 4 es: "+T4);
+        System.out.println ("Mi tarea 5 es: "+T5);
     
-    public stattic void main (String [] args) 
     
-     String cadenaTexto = "lista de tareas!!! %% $$$":
     
     
 }
